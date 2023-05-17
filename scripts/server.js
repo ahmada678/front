448ce81fbe35b657de4ef6b07a7518184977f085
Ahmad Ayii <ahmad.ayii94@gmail.com>
2023-05-17 21:31:29 +0300

    change

--- scripts/server.js
+++ scripts/server.js
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
-const socket = io(socketUrl);
-const socketUrl = import.meta.env.VITE_SOCKET_URL;
 
-// export const socket = io("http://localhost:5050");
+const socketURL = import.meta.env.VITE_SOCKET_SERVER;
+
+export const socket = io(socketURL);
